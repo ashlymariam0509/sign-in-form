97eb894b7bd194a712d85b4890de590a5c5747ba
ashlymariam0509 <ashlysank2006@gmail.com>
2025-04-05 16:14:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function clicksubmitbutton(event)
+{
+    event.preventDefault();
+    const username =document.getElementById("username").value;
+    const welcomeMessage =document.getElementById('welcome-message');
+    welcomeMessage.textContent='Welcome,'+username + '!';
+    welcomeMessage.style.display='block';
+
+}
